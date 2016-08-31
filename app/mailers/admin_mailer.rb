04dfcf9188194f95c913bff2734a7e72fed70fe0
Jared Cosulich <jared.cosulich@gmail.com>
2016-08-31 13:08:59 -0400

    don't email me when I create something

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,6 +3,7 @@ class AdminMailer < ApplicationMailer
   default to: 'jared@irrationaldesign.com', from: 'services@infiniteq.net'
 
   def object_created(object_created)
+    return if object_created.user.try(:id) == 1
     @object_created = object_created
     mail(subject: "InfiniteQ #{object_created.class.to_s} Created")
   end
