55494bf4bbcd43da52949d4b67c85c26b8008c09
Jared Cosulich <jared.cosulich@gmail.com>
2016-08-31 13:01:27 -0400

    more voting capybara tests

--- spec/feature/voting_spec.rb
+++ spec/feature/voting_spec.rb
@@ -10,7 +10,6 @@ feature "Voting", js: true do
     question = questions(:one)
 
     visit("/topics/#{topic.slug}")
-    assert page.has_content?('My Account')
 
     vote_total = find("#question-#{question.id} .vote-total .small")
     expect(vote_total).to have_content '1.9'
@@ -18,5 +17,42 @@ feature "Voting", js: true do
     click_button("vote-down-question-#{question.id}")
 
     expect(vote_total).to have_content '-0.1'
+  end
+
+  scenario "Voting up a question" do
+    sign_in users(:registered)
+
+    question = questions(:two)
+
+    visit("/questions/#{question.slug}")
+    expect(page).to have_content question.text
+    expect(page).to have_content question.details
+
+    vote_total = find("#question-#{question.id} .vote-total .small")
+    expect(vote_total).to have_content '0.1'
+
+    click_button("vote-up-question-#{question.id}")
+
+    expect(vote_total).to have_content '1.1'
+  end
+
+  scenario "Voting up an answer" do
+    sign_in users(:registered)
+
+    question = questions(:one)
+    answer = answers(:two)
+
+    visit("/questions/#{question.slug}")
+    expect(page).to_not have_content answer.text
+
+    click_link 'Unverified'
+    expect(page).to have_content answer.text
+
+    vote_total = find("#answer-#{answer.id} .vote-total .small")
+    expect(vote_total).to have_content '0.1'
+
+    click_button("vote-up-answer-#{answer.id}")
+
+    expect(vote_total).to have_content '1.1'
   end
 end
