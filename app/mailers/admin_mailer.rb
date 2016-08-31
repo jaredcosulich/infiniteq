a0dc7ff86cb0f2ff704e245dfcd94ed7911a9361
Jared Cosulich <jared.cosulich@gmail.com>
2016-08-31 13:15:18 -0400

    fixing admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,7 +3,7 @@ class AdminMailer < ApplicationMailer
   default to: 'jared@irrationaldesign.com', from: 'services@infiniteq.net'
 
   def object_created(object_created)
-    return if object_created.user.try(:id) == 1
+    return if object_created.try(:user).try(:id) == 1
     @object_created = object_created
     mail(subject: "InfiniteQ #{object_created.class.to_s} Created")
   end
