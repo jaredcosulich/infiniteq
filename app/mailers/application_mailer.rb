b47fe05b5c764290877ab8ccc90a1be92ed65802
Jared Cosulich <jared.cosulich@gmail.com>
2016-09-20 11:44:04 -0400

    adding in default bcc

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'support@infiniteq.net'
+  default from: 'support@infiniteq.net', bcc: 'infiniteq@irrationaldesign.com'
   layout 'mailer'
 end
