1a2bef849c3bc97e71fdf9ffccac2902e03df8ac
Jared Cosulich <jared.cosulich@gmail.com>
2016-09-20 11:10:42 -0400

    voting down is same as flagging

--- spec/feature/voting_spec.rb
+++ spec/feature/voting_spec.rb
@@ -14,9 +14,37 @@ feature "Voting", js: true do
     vote_total = find("#question-#{question.id} .vote-total .small")
     expect(vote_total).to have_content '1.9'
 
-    click_button("vote-down-question-#{question.id}")
+    find("#vote-down-question-#{question.id}").click
 
-    expect(vote_total).to have_content '-0.1'
+    within "##{question.total_identifier}verifiedtrue-flag-modal" do
+      expect(page).to have_content('Why are you voting down this question?')
+
+      6.times { choose('flag_reason_factually_incorrect') }
+      expect(find_field('flag_reason_factually_incorrect')).to be_checked
+
+      fill_in 'flag[details]', with: 'Because it is deserves a flag!'
+
+      6.times { choose 'flag_action_trust' }
+      expect(find_field('flag_action_trust')).to be_checked
+
+      within '#flag_trust' do
+        select('1 trust point')
+      end
+
+      click_button 'Vote Down'
+    end
+
+    wait_for_ajax
+    expect(page).to_not have_css('.fa-spin')
+
+    expect(page).to have_css("#question-#{question.id}")
+    within("#question-#{question.id}") do
+      expect(page).to_not have_content(1.9)
+      expect(vote_total).to have_content '0.9'
+      expect(page).to have_content('Reason: Factually Incorrect')
+    end
+
+
   end
 
   scenario "Voting up a question" do
